815719d9c3292b202197305918b11ae48c99301e
Vladimir Orlov <orelik392@gmail.com>
2024-02-12 15:26:23 +0200

    excersize

--- 03_08/script.js
+++ 03_08/script.js
@@ -19,7 +19,21 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+
+  newColor: function (color) {
+    this.color = color;
+  },
 };
 
 console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
+
+console.log("old left " + backpack.strapLength.left);
+
+backpack.newStrapLength(20, 22);
+
+console.log("new left " + backpack.strapLength.left);
+
+console.log("old color " + backpack.color);
+backpack.newColor("asdf");
+console.log("new color " + backpack.color);
